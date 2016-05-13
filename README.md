e0020a445fa8b180a376d85bccb294d5132cb201
Aakash Sur <aakash.sur@utexas.edu>
2016-05-13 13:00:34 -0700

    Change from D3 svg based vis to canvas based vis. Have not added the color scaling yet, more to come.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # hic
+
+A HTML5 visualization for Hi-C data. 
--- hic.js
+++ hic.js
@@ -1,49 +1,13 @@
-var data; // a global
-
-d3.json("Chr1.json", function(error, json) {
-  if (error) return console.warn(error);
-  data = json;
-
-  var zoom = d3.behavior.zoom()
-    .scaleExtent([1, 10])
-    .on("zoom", zoomed);
-
-  function zoomed() {
-    svg.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
-  }
-
-  var svg = d3.select('body')
-    .append('svg')
-    .attr('width', 2000)
-    .attr('height', 2000)
-    .append('g')
-    .call(zoom)
-    .append('g');
-
-  var size = 3;
-
-  var color = d3.scale.linear()
-    .domain([0,25])
-    .range(['white', 'red']);
-
-  var rectangles = svg.selectAll('rect')
-    .data(data)
-    .enter()
-    .append('rect')
-    .attr('x', function(d){return d.row  * size;})
-    .attr('y', function(d){return d.col  * size;})
-    .attr('width', size)
-    .attr('height', size)
-    .attr('fill', function(d){return color(d.count);})
-    .style("pointer-events", "all");
-
- 
-
-});
-
-
-
-
-
-
- 
+function draw() {
+  $.getJSON("Chr36.json", function(json) {
+    var canvas = document.getElementById("canvas");
+    if (canvas.getContext) {
+      var ctx = canvas.getContext("2d");
+      var size = 1;
+      $.each(json, function() {
+        ctx.fillStyle = "black";
+        ctx.fillRect (this.row * size, this.col * size, size, size);
+      });
+      }
+  });
+};
