1f3076e74af27177d809a4d27dbdf6dafd438ce0
Aakash Sur <aakash.sur@utexas.edu>
2016-05-13 13:04:53 -0700

    changed size

--- hic.js
+++ hic.js
@@ -3,7 +3,7 @@ function draw() {
     var canvas = document.getElementById("canvas");
     if (canvas.getContext) {
       var ctx = canvas.getContext("2d");
-      var size = 1;
+      var size = 3;
       $.each(json, function() {
         ctx.fillStyle = "black";
         ctx.fillRect (this.row * size, this.col * size, size, size);
